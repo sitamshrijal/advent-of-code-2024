884852641cec0e0d68fcfcdf81c0c811cb1de92e
Sitamsh Rijal <sitamshrijal@gmail.com>
2024-12-03 19:41:12 +0545

    Add solution for Day 3 Part 2

--- src/Day3.kt
+++ src/Day3.kt
@@ -12,7 +12,20 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-        return input.length
+        val regex = """mul\((\d+),(\d+)\)""".toRegex()
+        return regex.findAll(input)
+            .sumOf { result ->
+                val substringBefore = input.substringBefore(result.value)
+                val lastDo = substringBefore.lastIndexOf("do()")
+                val lastDont = substringBefore.lastIndexOf("don't()")
+
+                if (lastDo > lastDont || lastDo == -1) {
+                    val (a, b) = result.destructured
+                    a.toInt() * b.toInt()
+                } else {
+                    0
+                }
+            }
     }
 
     val input = Path("src/input3.txt").readText()
