a762bffaa7041f884c0931c26d9a26264f9fa6dd
Sitamsh Rijal <sitamshrijal@gmail.com>
2024-12-03 18:40:05 +0545

    Add solution for Day 2 Part 2

--- src/Day2.kt
+++ src/Day2.kt
@@ -7,21 +7,35 @@ fun main() {
             splits.map { it.toInt() }
         }
         return reports.count { report ->
-            val zipped = report.zipWithNext()
-
-            val allIncreasing = zipped.all { (first, second) -> first < second }
-            val allDecreasing = zipped.all { (first, second) -> first > second }
-            val diffs = zipped.map { (first, second) -> (first - second).absoluteValue }
-
-            (allIncreasing || allDecreasing) && diffs.all { it in 1..3 }
+            report.isSafe()
         }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val reports = input.map { line ->
+            val splits = line.split(" ")
+            splits.map { it.toInt() }
+        }
+        return reports.count { report ->
+            report.isSafeWithDampener()
+        }
     }
 
     val input = readInput("input2")
     part1(input).println()
     part2(input).println()
+}
+
+fun List<Int>.isSafe(): Boolean {
+    val zipped = zipWithNext()
+
+    val allIncreasing = zipped.all { (first, second) -> first < second }
+    val allDecreasing = zipped.all { (first, second) -> first > second }
+    val diffs = zipped.map { (first, second) -> (first - second).absoluteValue }
+
+    return (allIncreasing || allDecreasing) && diffs.all { it in 1..3 }
+}
+
+fun List<Int>.isSafeWithDampener(): Boolean = indices.any { toRemove ->
+    filterIndexed() { index, _ -> index != toRemove }.isSafe()
 }
